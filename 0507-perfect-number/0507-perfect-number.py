ce607b9e10c9912477ceb991fc087283d44ebfd9
Kumar Ansh Mishra <2k25cse2513471@gmail.com>
2026-07-27 12:31:59 +0530

    Time: 4 ms (80.24%), Space: 12.6 MB (43.94%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.py
@@ -0,0 +1,17 @@
+import math
+
+class Solution(object):
+    def checkPerfectNumber(self, num):
+        if num <= 1:
+            return False
+
+        s = 1
+
+        for i in range(2, int(math.sqrt(num)) + 1):
+            if num % i == 0:
+                s += i
+
+                if i != num // i:
+                    s += num // i
+
+        return s == num
