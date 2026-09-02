838a7ab08328470d4539cac6b656f31dbc02ea33
Kumar Ansh Mishra <2k25cse2513471@gmail.com>
2026-09-02 14:57:27 +0530

    Time: 47 ms (39.49%), Space: 19 MB (92.82%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.py
+++ 0278-first-bad-version/0278-first-bad-version.py
@@ -1,24 +1,13 @@
 # The isBadVersion API is already defined for you.
-# @param version, an integer
-# @return a bool
-# def isBadVersion(version):
+# def isBadVersion(version: int) -> bool:
 
-class Solution(object):
-    def firstBadVersion(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
         left, right = 1, n
-        
         while left < right:
             mid = left + (right - left) // 2
-            
             if isBadVersion(mid):
-                # The first bad version is mid or earlier
                 right = mid
             else:
-                # The first bad version is strictly after mid
                 left = mid + 1
-                
         return left
