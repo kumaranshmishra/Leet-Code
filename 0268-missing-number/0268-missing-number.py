618511cc1d5995ae82e7295e2b03f0b97d65b93d
Kumar Ansh Mishra <2k25cse2513471@gmail.com>
2026-09-08 10:39:51 +0530

    Time: 4170 ms (5.09%), Space: 20.4 MB (75.51%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,6 +1,8 @@
-class Solution(object):
-    def missingNumber(self, nums):
-         n = len(nums)
-         expected_sum = n * (n + 1) // 2
-         actual_sum = sum(nums)
-         return expected_sum - actual_sum
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        for i in range (len(nums)+1):
+            if nums.count(i) :
+                continue
+            else :
+                return i    
+        
